608eda91279292bcafe70159dbc3f5c1e30f8142
Sébastien Vitard <contact@sebastienvitard.name>
2014-11-17 12:50:48 +0100

    Merge WebServicesTask

--- Webservices/Android/Webservices/src/fr/haploid/WebservicesPlugin/WebservicesTask.java
+++ Webservices/Android/Webservices/src/fr/haploid/WebservicesPlugin/WebservicesTask.java
@@ -1,125 +1,178 @@
+/**
+ *
+ * WebservicesTask
+ * WebservicesPlugin
+ *
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2014 Haploid
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ */
+
 package fr.haploid.WebservicesPlugin;
+
+import fr.cobaltians.cobalt.Cobalt;
+import fr.cobaltians.cobalt.fragments.CobaltFragment;
 
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.util.Log;
-import fr.cobaltians.cobalt.Cobalt;
-import fr.cobaltians.cobalt.fragments.CobaltFragment;
-import org.json.JSONArray;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.net.*;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-
-/**
- * Created by sebastienfamel on 04/11/2014.
- */
-public class WebservicesTask extends AsyncTask<JSONObject, Void, JSONObject> {
+final class WebservicesTask extends AsyncTask<Void, Void, JSONObject> {
 
     private static final String TAG = WebservicesTask.class.getSimpleName();
 
-    private CobaltFragment mFragment;
+    private static final String JSPluginNameWebservices = "webservices";
 
-    private static final String CALL_ID = "callId";
-    private static final String CALL_WS = "callWS";
-    private static final String ON_STORAGE_RESULT = "onStorageResult";
-    private static final String ON_STORAGE_ERROR = "onStorageError";
-    private static final String ON_WS_ERROR = "onWSError";
-    private static final String ON_WS_RESULT = "onWSResult";
-    private static final String PARAMS = "params";
-    private static final String PROCESS_DATA = "processData";
-    private static final String SAVE_TO_STORAGE = "saveToStorage";
-    private static final String SEND_CACHE_RESULT = "sendCacheResult";
-    private static final String STATUS_CODE = "statusCode";
-    private static final String STORAGE_KEY = "storageKey";
-    private static final String TEXT = "text";
-    private static final String TYPE = "type";
-    private static final String URL = "url";
-    private static final String WEBSERVICES = "webservices";
-    private static final String WS_SUCCESS = "ws_success";
+    private static final String kJSCallId = "callId";
+    private static final String kJSSendCacheResult = "sendCacheResult";
+    private static final String kJSStorageKey = "storageKey";
+    private static final String kJSProcessData = "processData";
+    private static final String kJSUrl = "url";
+    private static final String kJSParams = "params";
+    private static final String kJSType = "type";
+    private static final String kJSSaveToStorage = "saveToStorage";
 
-    /**************
-    * Constructor
-    **************/
-    public WebservicesTask (CobaltFragment fragment) {
+    private static final String kJSSuccess = "ws_success";
+    private static final String kJSStatusCode = "statusCode";
+    private static final String kJSText = "text";
+
+    private static final String JSActionOnStorageResult = "onStorageResult";
+    private static final String JSActionOnStorageError = "onStorageError";
+    private static final String JSTextEmpty = "EMPTY";
+    private static final String JSTextNotFound = "NOT_FOUND";
+    private static final String JSTextUnknownError = "UNKNOW_ERROR";
+
+    private static final String JSOnWSResult = "onWSResult";
+    private static final String JSOnWSError = "onWSError";
+
+    private final CobaltFragment mFragment;
+    private long mCallId;
+    private boolean mSendCacheResult;
+    private String mStorageKey;
+    private JSONObject mProcessData;
+    private String mUrl;
+    private String mParams;
+    private String mType;
+    private boolean mSaveToStorage;
+
+    public WebservicesTask(CobaltFragment fragment, JSONObject message) {
         mFragment = fragment;
+
+        try {
+            mCallId = message.getLong(kJSCallId);
+            JSONObject data = message.getJSONObject(Cobalt.kJSData);
+            mSendCacheResult = data.optBoolean(kJSSendCacheResult);
+            mUrl = data.optString(kJSUrl, null);
+
+            if (mUrl != null) {
+                mParams = data.optString(kJSParams);
+                mType = data.getString(kJSType);
+                mSaveToStorage = data.optBoolean(kJSSaveToStorage);
+            }
+
+            if (mSendCacheResult
+                || (mUrl != null && mSaveToStorage)) {
+                mStorageKey = data.getString(kJSStorageKey);
+                mProcessData = data.optJSONObject(kJSProcessData);
+            }
+        }
+        catch (JSONException exception) {
+            if (Cobalt.DEBUG) {
+                Log.e(WebservicesPlugin.TAG,    TAG + ": check your Webservice call. Known issues: \n"
+                                                + "\t- missing data field, \n"
+                                                + "\t- url field is defined but but missing type field, \n"
+                                                + "\t- sendCacheResult field is true or url field is defined and saveToStorage field is true but missing storageKey field.\n");
+                exception.printStackTrace();
+            }
+        }
     }
 
     @Override
-    protected JSONObject doInBackground(JSONObject... jsonObjects) {
-        if (jsonObjects.length == 0) {
-            return null;
-        }
-        JSONObject jsonObject = jsonObjects[0];
-
-        HttpURLConnection urlConnection = null;
-        BufferedReader reader = null;
-        JSONObject responseHolder = new JSONObject();
-
-
-        WebservicesResponseData responseData = null;
-
+    protected JSONObject doInBackground(Void... params) {
         try {
-            long callId = jsonObject.getLong(CALL_ID);
+            if (mSendCacheResult) {
+                JSONObject cacheResultMessage = new JSONObject();
+                cacheResultMessage.put(Cobalt.kJSType, Cobalt.JSTypePlugin);
+                cacheResultMessage.put(Cobalt.kJSPluginName, JSPluginNameWebservices);
 
-            responseHolder.put(CALL_ID, callId );
-            responseHolder.put(WS_SUCCESS, false);
-            responseHolder.put(CALL_WS, false);
-            responseHolder.put(TEXT, "" );
+                int storageSize = WebservicesData.getCountItem();
+                if (storageSize > 0) {
+                    if (mStorageKey != null) {
+                        String storedValue = WebservicesPlugin.storedValueForKey(mStorageKey, mFragment);
 
-            JSONObject data = jsonObject.getJSONObject(Cobalt.kJSData);
-            Log.d(TAG, "data on start = "+data.toString());
-            boolean sendCacheResult = data.optBoolean(SEND_CACHE_RESULT, false);
+                        if (storedValue != null) {
+                            try {
+                                JSONObject storedData = new JSONObject(storedValue);
+                                storedData = WebservicesPlugin.treatData(storedData, mProcessData, mFragment);
+                                storedData.put(kJSCallId, mCallId);
 
-            if (sendCacheResult) {
-                JSONObject resultWs = new JSONObject();
-                resultWs.put(Cobalt.kJSType, Cobalt.JSTypePlugin);
-                resultWs.put(Cobalt.kJSPluginName, WEBSERVICES);
-                try {
-                    int storageSize = WebservicesData.getCountItem();
-                    if (storageSize > 0) {
+                                cacheResultMessage.put(Cobalt.kJSAction, JSActionOnStorageResult);
+                                cacheResultMessage.put(Cobalt.kJSData, storedData);
+                            }
+                            catch (JSONException exception) {
+                                if (Cobalt.DEBUG) {
+                                    Log.e(WebservicesPlugin.TAG, TAG + " - doInBackground: value parsing failed for key " + mStorageKey + ". Value: " + storedValue);
+                                    exception.printStackTrace();
+                                }
 
-                        String storedByKey = WebservicesPlugin.storedValueForKey(data.getString(STORAGE_KEY), mFragment);
-
-                        if (storedByKey != null) {
-                            JSONObject dataFromStorage = new JSONObject(storedByKey);
-                            dataFromStorage = WebservicesPlugin.treatData(dataFromStorage, data.optJSONObject(PROCESS_DATA), mFragment);
-                            dataFromStorage.put(CALL_ID, callId);
-                            resultWs.put(Cobalt.kJSAction, ON_STORAGE_RESULT);
-                            resultWs.put(Cobalt.kJSData, dataFromStorage);
+                                cacheResultMessage.put(Cobalt.kJSAction, JSActionOnStorageError);
+                                cacheResultMessage.put(kJSText, JSTextUnknownError);
+                            }
                         }
                         else {
-                            resultWs.put(Cobalt.kJSAction, ON_STORAGE_ERROR);
-                            resultWs.put(TEXT, "NOT_FOUND");
+                            if (Cobalt.DEBUG) Log.w(WebservicesPlugin.TAG, TAG + " - doInBackground: value not found in cache for key " + mStorageKey + ".");
+
+                            cacheResultMessage.put(Cobalt.kJSAction, JSActionOnStorageError);
+                            cacheResultMessage.put(kJSText, JSTextNotFound);
                         }
                     }
                     else {
-                        resultWs.put(Cobalt.kJSAction, ON_STORAGE_ERROR);
-                        resultWs.put(TEXT, "EMPTY");
+                        cacheResultMessage.put(Cobalt.kJSAction, JSActionOnStorageError);
+                        cacheResultMessage.put(kJSText, JSTextUnknownError);
                     }
                 }
-                catch (JSONException e) {
-                    resultWs.put(Cobalt.kJSAction, ON_STORAGE_ERROR);
-                    resultWs.put(TEXT, "UNKNOW_ERROR");
+                else {
+                    if (Cobalt.DEBUG) Log.w(WebservicesPlugin.TAG, TAG + " - doInBackground: cache is empty.");
+
+                    cacheResultMessage.put(Cobalt.kJSAction, JSActionOnStorageError);
+                    cacheResultMessage.put(kJSText, JSTextEmpty);
                 }
-                mFragment.sendMessage(resultWs);
+
+                mFragment.sendMessage(cacheResultMessage);
             }
 
-            if (data.has(URL)) {
-                // Note: it's optional from Web with default value = false
-                boolean saveToStorage = data.getBoolean(SAVE_TO_STORAGE);
-                if (saveToStorage) {
-                    responseHolder.put(STORAGE_KEY, data.getString(STORAGE_KEY));
-                }
-
-                responseHolder.put(SAVE_TO_STORAGE, saveToStorage);
-
+            if (mUrl != null
+                && mType != null) {
+                // TODO: later, use Helper
+                /*
                 responseData = WebservicesHelper.downloadFromServer(data.getString(URL), data.getJSONObject(PARAMS), data.getString(TYPE));
                 if (responseData != null) {
                     responseHolder.put(TEXT, responseData.getResponseData());
@@ -127,60 +180,119 @@ public class WebservicesTask extends AsyncTask<JSONObject, Void, JSONObject> {
                     responseHolder.put(WS_SUCCESS, responseData.isSuccess());
                     responseHolder.put(CALL_WS, true);
                 }
+                */
+
+                Uri.Builder builder = new Uri.Builder();
+                builder.encodedPath(mUrl);
+
+                if (mParams != null) builder.encodedQuery(mParams);
+
+                JSONObject response = new JSONObject();
+                response.put(kJSSuccess, false);
+
+                try {
+                    URL url = new URL(builder.build().toString());
+
+                    try {
+
+                        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                        urlConnection.setRequestMethod(mType);
+                        urlConnection.connect();
+
+                        int responseCode = urlConnection.getResponseCode();
+                        response.put(kJSStatusCode, responseCode);
+
+                        InputStream inputStream = urlConnection.getInputStream();
+                        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+                        StringBuffer buffer = new StringBuffer();
+                        String line;
+
+                        while ((line = reader.readLine()) != null) {
+                            buffer.append(line).append("\n");
+                        }
+
+                        if (buffer.length() != 0) response.put(kJSText, buffer.toString());
+
+                        response.put(kJSSuccess, true);
+
+                        return response;
+                    }
+                    catch (ProtocolException exception) {
+                        if (Cobalt.DEBUG) {
+                            Log.e(WebservicesPlugin.TAG, TAG + " - doInBackground: not supported request method type " + mType);
+                            exception.printStackTrace();
+                        }
+                    }
+                    catch (IOException exception) {
+                        exception.printStackTrace();
+                    }
+                }
+                catch (MalformedURLException exception) {
+                    if (Cobalt.DEBUG) {
+                        Log.e(WebservicesPlugin.TAG, TAG + " - doInBackground: malformed url " + builder.build().toString() + ".");
+                        exception.printStackTrace();
+                    }
+                }
             }
-            return responseHolder;
         }
-        catch (JSONException e) {
-            e.printStackTrace();
-            return responseHolder;
+        catch (JSONException exception) {
+            exception.printStackTrace();
         }
+
+        return null;
     }
 
     @Override
-    protected void onPostExecute(JSONObject jsonObject) {
-        super.onPostExecute(jsonObject);
-        JSONObject resultWs = new JSONObject();
-        try {
-            if (jsonObject.getBoolean(CALL_WS)) {
-                resultWs.put(Cobalt.kJSType, Cobalt.JSTypePlugin);
-                resultWs.put(Cobalt.kJSPluginName, WEBSERVICES);
+    protected void onPostExecute(JSONObject response) {
+        super.onPostExecute(response);
 
-                if (jsonObject.getBoolean(WS_SUCCESS)) {
-                    resultWs.put(Cobalt.kJSAction, ON_WS_RESULT);
-                }
+        if (response != null) {
+            try {
+                JSONObject message = new JSONObject();
+                message.put(Cobalt.kJSType, Cobalt.JSTypePlugin);
+                message.put(Cobalt.kJSPluginName, JSPluginNameWebservices);
+
+                if (response.getBoolean(kJSSuccess)) message.put(Cobalt.kJSAction, JSOnWSResult);
                 else {
-                    resultWs.put(Cobalt.kJSAction, ON_WS_ERROR);
-                }
-
-                int statusCode = jsonObject.optInt(STATUS_CODE, -1);
-                if (statusCode != -1) {
-                    resultWs.put(STATUS_CODE, statusCode);
+                    message.put(Cobalt.kJSAction, JSOnWSError);
+                    int statusCode = response.optInt(kJSStatusCode, -1);
+                    if (statusCode != -1) message.put(kJSStatusCode, statusCode);
                 }
 
                 JSONObject data = new JSONObject();
-                data.put(CALL_ID, jsonObject.getLong(CALL_ID));
+                data.put(kJSCallId, mCallId);
 
-                try {
-                    JSONObject responseData = new JSONObject(jsonObject.getString(TEXT));
-                    responseData = WebservicesPlugin.treatData(responseData, jsonObject.optJSONObject(PROCESS_DATA), mFragment);
-                    data.put(Cobalt.kJSData, responseData);
+                String text = response.optString(kJSText, null);
+                if (text != null) {
+                    try {
+                        JSONObject responseData = new JSONObject(text);
+                        responseData = WebservicesPlugin.treatData(responseData, mProcessData, mFragment);
+                        data.put(Cobalt.kJSData, responseData);
+                    }
+                    catch (JSONException exception) {
+                        Log.w(Cobalt.TAG, TAG + " - onPostExecute: response could not be parsed as JSON. Response: " + text);
+                        exception.printStackTrace();
+                        data.put(kJSText, text);
+                    }
                 }
-                catch (JSONException e) {
-                    data.put(TEXT, jsonObject.get(TEXT));
+
+                message.put(Cobalt.kJSData, data);
+                mFragment.sendMessage(message);
+
+                if (mSaveToStorage && mStorageKey != null) {
+                    WebservicesPlugin.storeValue(data.toString(), mStorageKey, mFragment);
                 }
-
-                resultWs.put(Cobalt.kJSData, data);
-
-                if (jsonObject.optBoolean(SAVE_TO_STORAGE)) {
-                    WebservicesPlugin.storeValue(data.toString(), jsonObject.getString(STORAGE_KEY), mFragment);
+                else if (Cobalt.DEBUG) {
+                    Log.w(Cobalt.TAG,   TAG + " - onPostExecute: missing storageKey field \n"
+                                        + "Web service response will not be stored in cache.");
                 }
-
-                mFragment.sendMessage(resultWs);
+            }
+            catch (JSONException exception) {
+                exception.printStackTrace();
             }
         }
-        catch (JSONException e) {
-            e.printStackTrace();
+        else if (Cobalt.DEBUG) {
+            Log.d(Cobalt.TAG, TAG + " - onPostExecute: url and/or type fields were empty, Webservice has not been called.");
         }
-
     }
 }
