82648203f6825d8e8d9b687769dcab1967ddd772
Guillaume Gendre <dzb.rtz@gmail.com>
2015-07-30 09:29:15 +0200

    implemented publish, subscribe and unsubscribe on the web side

--- PubSub/Web/cobalt.pubsub.js
+++ PubSub/Web/cobalt.pubsub.js
@@ -4,14 +4,32 @@
 
         init:function(options){
             //create shortcuts
-            
-			
-            if (options){
-                this.config(options);
-            }
+            cobalt.publish=this.publish.bind(this);
+            cobalt.subscribe=this.subscribe.bind(this);
+            cobalt.unsubscribe=this.unsubscribe.bind(this);
 
         },
-        config:function(settings){
+        publish:function(channel, message){
+            if (typeof channel == "string" && typeof message == "object"){
+                this.send("publish", { channel : channel, message : message });
+            }else{
+                cobalt.log("PubSub : channel must be a string and message should be an object");
+            }
+        },
+        subscribe:function(channel, callback){
+            var callback_id = cobalt.registerCallback(callback)
+            if (typeof channel == "string" && typeof callback_id == "string"){
+                this.send("subscribe", { channel : channel, callback : callback_id });
+            }else{
+                cobalt.log("PubSub : channel must be a string and callback should be set");
+            }
+        },
+        unsubscribe:function(channel){
+            if (typeof channel == "string"){
+                this.send("unsubscribe", { channel : channel });
+            }else{
+                cobalt.log("PubSub : channel must be a string");
+            }
         },
         handleEvent:function(json){
             cobalt.log(this.name, ' plugin : unknown event received :', json)
